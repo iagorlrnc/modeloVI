3728c89a308a159de9c7d286d74908c32104df6c
iagor lourenco <117778088+iagorlrnc@users.noreply.github.com>
2025-09-25 11:56:45 -0300

    updateNameInAbout

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -39,9 +39,9 @@ const About: React.FC = () => {
               <div className="bg-white p-6 rounded-xl border border-rosa-marfil-2 shadow-sm">
                 <div className="flex items-center space-x-3 mb-3">
                   <User className="w-6 h-6 text-accent" />
-                  <h4 className="font-semibold text-text">Orientadora</h4>
+                  <h4 className="font-semibold text-text">Aluna</h4>
                 </div>
-                <p className="text-gray-600">Prof.ª Dra. Maria Silva Santos</p>
+                <p className="text-gray-600">Laís Pereira Dias</p>
               </div>
               
               <div className="bg-white p-6 rounded-xl border border-rosa-marfil-2 shadow-sm">
@@ -49,7 +49,7 @@ const About: React.FC = () => {
                   <GraduationCap className="w-6 h-6 text-accent" />
                   <h4 className="font-semibold text-text">Instituição</h4>
                 </div>
-                <p className="text-gray-600">Universidade Federal de São Paulo</p>
+                <p className="text-gray-600">Universidade Federal do Tocantins</p>
               </div>
             </div>
           </div>
